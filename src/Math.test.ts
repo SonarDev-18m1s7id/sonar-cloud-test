2726b8a2cc52f538986efa37a528c2de52e44793
Daniel Nagy <1622446+daniel-nagy@users.noreply.github.com>
2024-12-02 14:26:30 -0600

    rename module

--- src/Math.test.ts
+++ src/Math.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from "bun:test";
-import { add } from "./test";
+import { add } from "./Math";
 
 test("adding 2 numbers returns their sum", () => {
   expect(add(1, 2)).toBe(3);
