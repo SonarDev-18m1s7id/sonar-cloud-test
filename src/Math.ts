c3fa78fc0764261db251a32328a3f3a1b1418095
Daniel Nagy <1622446+daniel-nagy@users.noreply.github.com>
2024-12-16 15:37:03 -0600

    new issue

--- src/Math.ts
+++ src/Math.ts
@@ -5,5 +5,6 @@ export function add(a: number, b: number): number {
 export function subtract(a: number, b: number): number {
   console.log(subtract.caller);
   console.log(subtract.arguments);
+  const zzz = "zzz";
   return a - b;
 }
